c08e08fc889f1c29499f41f6d9d18cc89dead20e
Javi Martinez <info@axxusentertainment.com>
2026-08-24 17:36:32 +0200

    Updated npc_warlock_pet_renamer.cpp

--- src/npc_warlock_pet_renamer.cpp
+++ src/npc_warlock_pet_renamer.cpp
@@ -22,8 +22,10 @@ class npc_warlock_pet_renamer : public CreatureScript
 private:
     static constexpr int VISUAL_FEEDBACK_SPELL_ID = 46331;
     static constexpr int64 RENAME_COOLDOWN_SECONDS = 30;
+    static constexpr uint32 RENAME_COST_COPPER = 15 * 10000;
 
     static std::unordered_map<uint64, int64> _lastRenameByPlayer;
+    static std::unordered_map<uint64, std::string> _proposedName;
     static std::mutex _renameMutex;
 
     static bool TryStartRename(Player* player)
@@ -67,7 +69,36 @@ private:
         name[0] = std::toupper(static_cast<unsigned char>(name[0]));
     }
 
-    static void HandlePetRename(Player* player, const char* nameStr)
+    static bool ValidateProposedName(Player* player, Pet* pet, std::string& name)
+    {
+        NormalizeName(name);
+
+        PetNameInvalidReason res = ObjectMgr::CheckPetName(name);
+        if (res != PET_NAME_SUCCESS)
+        {
+            player->GetSession()->SendPetNameInvalid(res, name, nullptr);
+            return false;
+        }
+
+        if (sObjectMgr->IsReservedName(name))
+        {
+            player->GetSession()->SendPetNameInvalid(PET_NAME_RESERVED, name, nullptr);
+            return false;
+        }
+
+        if (sObjectMgr->IsProfanityName(name))
+        {
+            player->GetSession()->SendPetNameInvalid(PET_NAME_PROFANE, name, nullptr);
+            return false;
+        }
+
+        if (pet->GetName() == name)
+            return false;
+
+        return true;
+    }
+
+    static void ExecuteRename(Player* player, const std::string& name)
     {
         if (player->getClass() != CLASS_WARLOCK)
             return;
@@ -76,43 +107,25 @@ private:
         if (!pet)
             return;
 
+        std::string oldName = pet->GetName();
+        if (oldName == name)
+            return;
+
+        if (player->GetMoney() < RENAME_COST_COPPER)
+        {
+            ChatHandler(player->GetSession()).SendSysMessage("You don't have enough gold. The rename costs 15 Gold.");
+            return;
+        }
+
         if (!TryStartRename(player))
         {
             ChatHandler(player->GetSession()).SendSysMessage("You must wait a moment before renaming your pet again.");
             return;
         }
 
-        if (!nameStr)
-            return;
-
-        std::string name(nameStr);
-        NormalizeName(name);
-
-        PetNameInvalidReason res = ObjectMgr::CheckPetName(name);
-        if (res != PET_NAME_SUCCESS)
-        {
-            player->GetSession()->SendPetNameInvalid(res, name, nullptr);
-            return;
-        }
-
-        if (sObjectMgr->IsReservedName(name))
-        {
-            player->GetSession()->SendPetNameInvalid(PET_NAME_RESERVED, name, nullptr);
-            return;
-        }
-
-        if (sObjectMgr->IsProfanityName(name))
-        {
-            player->GetSession()->SendPetNameInvalid(PET_NAME_PROFANE, name, nullptr);
-            return;
-        }
-
-        std::string oldName = pet->GetName();
-        if (oldName == name)
-            return;
-
         uint32 petNumber = pet->GetCharmInfo()->GetPetNumber();
 
+        player->ModifyMoney(-static_cast<int32>(RENAME_COST_COPPER));
         pet->SetName(name);
         player->CastSpell(pet, VISUAL_FEEDBACK_SPELL_ID, true);
 
@@ -122,7 +135,7 @@ private:
         stmt->SetData(2, petNumber);
         CharacterDatabase.Execute(stmt);
 
-        LOG_DEBUG("entities.pet.renamer", "Player {} renamed pet #{} from '{}' to '{}'",
+        LOG_DEBUG("entities.pet.renamer", "Player {} renamed pet #{} from '{}' to '{}' for 15 gold",
             player->GetName(), petNumber, oldName, name);
 
         // UNIT_FIELD_PET_NAME_TIMESTAMP is a 32-bit protocol field and cannot be widened;
@@ -167,6 +180,11 @@ public:
 
     bool OnGossipHello(Player* player, Creature* creature) override
     {
+        {
+            std::lock_guard<std::mutex> lock(_renameMutex);
+            _proposedName.erase(player->GetGUID().GetCounter());
+        }
+
         if (player->getClass() != CLASS_WARLOCK)
             AddGossipItemFor(player, GOSSIP_ICON_CHAT, "|cffb50505WARLOCKS ONLY|r", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
         else
@@ -177,7 +195,7 @@ public:
             else
             {
                 AddGossipItemFor(player, GOSSIP_ICON_CHAT, "Current pet: " + GetPetInfo(pet), GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
-                AddGossipItemFor(player, GOSSIP_ICON_TALK, "Rename current pet", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3, "Type in your desired pet name in the next popup!", 0, true);
+                AddGossipItemFor(player, GOSSIP_ICON_TALK, "Rename current pet (15 Gold)", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3, "Type in your desired pet name in the next popup! Cost: 15 Gold", 0, true);
             }
         }
 
@@ -198,15 +216,79 @@ public:
             CloseGossipMenuFor(player);
             return true;
         }
+        else if (action == GOSSIP_ACTION_INFO_DEF + 4)
+        {
+            uint64 guid = player->GetGUID().GetCounter();
+            std::string name;
+            {
+                std::lock_guard<std::mutex> lock(_renameMutex);
+                auto it = _proposedName.find(guid);
+                if (it != _proposedName.end())
+                {
+                    name = it->second;
+                    _proposedName.erase(it);
+                }
+            }
+
+            if (!name.empty())
+                ExecuteRename(player, name);
+
+            CloseGossipMenuFor(player);
+            return true;
+        }
+        else if (action == GOSSIP_ACTION_INFO_DEF + 5)
+        {
+            std::lock_guard<std::mutex> lock(_renameMutex);
+            _proposedName.erase(player->GetGUID().GetCounter());
+            CloseGossipMenuFor(player);
+            return true;
+        }
 
         CloseGossipMenuFor(player);
         return false;
     }
 
-    bool OnGossipSelectCode(Player* player, Creature* /*creature*/, uint32 /*sender*/ , uint32 action, const char* code) override
+    bool OnGossipSelectCode(Player* player, Creature* creature, uint32 /*sender*/, uint32 action, const char* code) override
     {
         if (action == GOSSIP_ACTION_INFO_DEF + 3)
-            HandlePetRename(player, code);
+        {
+            if (player->getClass() != CLASS_WARLOCK)
+            {
+                CloseGossipMenuFor(player);
+                return true;
+            }
+
+            Pet* pet = GetAllowedPetForRename(player);
+            if (!pet)
+            {
+                CloseGossipMenuFor(player);
+                return true;
+            }
+
+            if (!code)
+            {
+                CloseGossipMenuFor(player);
+                return true;
+            }
+
+            std::string name(code);
+            if (!ValidateProposedName(player, pet, name))
+            {
+                CloseGossipMenuFor(player);
+                return true;
+            }
+
+            {
+                std::lock_guard<std::mutex> lock(_renameMutex);
+                _proposedName[player->GetGUID().GetCounter()] = name;
+            }
+
+            ClearGossipMenuFor(player);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "Confirm: Rename pet to '" + name + "' for 15 Gold?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "Cancel", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 5);
+            SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
+            return true;
+        }
 
         CloseGossipMenuFor(player);
         return true;
@@ -214,6 +296,7 @@ public:
 };
 
 std::unordered_map<uint64, int64> npc_warlock_pet_renamer::_lastRenameByPlayer;
+std::unordered_map<uint64, std::string> npc_warlock_pet_renamer::_proposedName;
 std::mutex npc_warlock_pet_renamer::_renameMutex;
 
 void AddSC_npc_warlock_pet_renamer()
