1e4d53ed56bd4ccb08100cb54f4b6f4072d2ae26
Javi Martinez <info@axxusentertainment.com>
2026-09-02 23:01:51 +0200

    Updated WPR_2024_03_28_02.sql

--- data/sql/db-world/updates/WPR_2024_03_28_02.sql
+++ data/sql/db-world/updates/WPR_2024_03_28_02.sql
@@ -2,16 +2,15 @@
 -- Locations matched from custom_spawns.sql
 
 SET @Entry = 200002;
-SET @GUID1 = 5300833;
-SET @GUID2 = 5300835;
 
 START TRANSACTION;
 
-DELETE FROM `creature` WHERE `guid` IN (@GUID1, @GUID2);
-DELETE FROM `creature_addon` WHERE `guid` IN (@GUID1, @GUID2);
+DELETE FROM `creature` WHERE `id` = @Entry
+    AND `position_x` IN (-8933.52, 1784.03)
+    AND `position_y` IN (967.673, -4357.46);
 
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(@GUID1, @Entry, 0, 0, 0, 1, 1, 0, -8933.52, 967.673, 117.179, 4.65933, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0, NULL),
-(@GUID2, @Entry, 1, 0, 0, 1, 1, 0, 1784.03, -4357.46, -14.3048, 0.137393, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+INSERT INTO `creature` (`id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(@Entry, 0, 0, 0, 1, 1, 0, -8933.52, 967.673, 117.179, 4.65933, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0, NULL),
+(@Entry, 1, 0, 0, 1, 1, 0, 1784.03, -4357.46, -14.3048, 0.137393, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
 
 COMMIT;
